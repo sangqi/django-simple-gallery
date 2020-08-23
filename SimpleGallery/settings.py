da1e47b1d61fc4beb7f30a7cf7020e8c9d4f9f56
Chenguang Liu <cliu4@snapchat.com>
2020-08-23 14:35:37 -0700

    update allowed host

--- SimpleGallery/settings.py
+++ SimpleGallery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2mk79!)ct-sfhc!ur(x*6f(&2jh&*33r*od6pl4#z)q&w3b1kk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ["django-simple-gallery.herokuapp.com", "localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
